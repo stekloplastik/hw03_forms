8fc86fae3f102601f7608f82ff639c64e1f8b465
Николай <slonopatam52@gmail.com>
2021-01-26 21:43:03 +0300

    2nd review

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
-
-from .models import Post, Group
-
-from .forms import PostForm
-
 from django.contrib.auth.decorators import login_required
+from .models import Post, Group
+from .forms import PostForm
 
 
 def index(request):
@@ -20,13 +17,11 @@ def group_posts(request, slug):
 
 @login_required
 def new_post(request):
+    form = PostForm(request.POST or None)
     if request.method == 'POST':
-        form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.save()
             return redirect('index')
-        return render(request, 'new.html', {'form': form})
-    form = PostForm()
     return render(request, 'new.html', {'form': form})
--- users/views.py
+++ users/views.py
@@ -1,24 +1,6 @@
-#  импортируем CreateView, чтобы создать ему наследника
 from django.views.generic import CreateView
-
-#  функция reverse_lazy позволяет получить URL по параметру
-# "name" функции path()
-#  берём, тоже пригодится
 from django.urls import reverse_lazy
-
-#  импортируем класс формы, чтобы сослаться на неё во view-классе
 from .forms import CreationForm
-
-from django.core.mail import send_mail
-
-
-send_mail(
-    'Тема письма',
-    'Текст письма.',
-    'from@example.com',  # Это поле "От кого"
-    ['to@example.com'],  # Это поле "Кому" (можно указать список адресов)
-    fail_silently=False,  # Сообщать об ошибках («молчать ли об ошибках?»)
-)
 
 
 class SignUp(CreateView):
